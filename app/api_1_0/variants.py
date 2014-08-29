85666d36ac76b77635532e797d3ebe88fbcb671a
nkenealy <neilkenealy>
2014-08-29 09:13:47 +0100

    prepared product variant api for receipt of variants

--- app/api_1_0/variants.py
+++ app/api_1_0/variants.py
@@ -66,13 +66,13 @@ def get_product_variants(id):
     })
 
 
-@api.route('/products/<int:id>/variants/', methods=['POST'])
+@api.route('/products/variants/', methods=['POST'])
 #TODO: put permission back on and make it product or variant specific
 #@permission_required(Permission.COMMENT)
 def new_product_variant(id):
     variant = Variant.from_json(request.json)
     #product = Product.query.get_or_404(id)
-    #product = Product.query.filter_by(pos_product_id=variant.pos_product_id)
+    product = Product.query.filter_by(pos_product_id=variant.pos_product_id)
     variant.product = product
     db.session.add(variant)
     db.session.commit()
