f58570c54676f574d7d03fb05469fbb253b6fe58
nkenealy <neilkenealy>
2014-08-30 07:56:05 +0100

    variant.pos_product_id

--- app/api_1_0/variants.py
+++ app/api_1_0/variants.py
@@ -70,8 +70,8 @@ def get_product_variants(id):
 #TODO: put permission back on and make it product or variant specific
 #@permission_required(Permission.COMMENT)
 def new_product_variant():
-    product = Product.query.filter_by(pos_product_id=4104).first()
     variant = Variant.from_json(request.json)
+    product = Product.query.filter_by(pos_product_id=variant.pos_product_id).first()
     #product = Product.query.get_or_404(id)
     #product = Product.query('select * from products where pos_product_id = variant.pos_product_id')
     #variant.product = product
